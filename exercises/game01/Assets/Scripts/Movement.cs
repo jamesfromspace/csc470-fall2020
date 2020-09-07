868c194d38022d0d2b396a2993516426e97fb092
James White <42709165+jamesfromspace@users.noreply.github.com>
2020-09-07 13:01:09 -0400

    updated game

---
+++ exercises/game01/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform target;
+   
+    public float speed = 4.0f;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+} 
