11c563bdd9179ece3dbe93fa8e0d5d9bc4e471da
James White <42709165+jamesfromspace@users.noreply.github.com>
2020-09-18 00:21:39 -0400

    update

---
+++ exercises/game02/Assets/Scripts/Laser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    GameObject muzzle;
+    Vector3 direction;
+    GameObject gReference;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        gReference = GameObject.FindGameObjectWithTag("Plane");
+        muzzle = GameObject.Find("muzzle");
+        direction = muzzle.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0f, 0f, 1f * Time.deltaTime);
+        Rigidbody rboby = GetComponent<Rigidbody>();
+        rboby.velocity = direction * 40f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("stormtrooper"))
+        {
+            
+            Destroy(other.gameObject);
+
+            GameManager.score += 1;
+        }
+    }
+}
---
+++ exercises/game02/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	float speed;
+	
+
+	// This value is set in the Unity editor by dragging the Text object
+	// into the slot in the inspector.
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+	void Update()
+	{
+		speed = 15.0f;
+     
+    	if (Input.GetKey(KeyCode.D)) {
+       		transform.Translate(Vector3.right * speed * Time.deltaTime);
+    	}
+		if (Input.GetKey(KeyCode.A)) {
+       		transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		
+	}
+}
