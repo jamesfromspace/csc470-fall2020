70e19feedbf1cf9a6644519077df9612944b0a7d
James White <42709165+jamesfromspace@users.noreply.github.com>
2020-11-12 00:40:23 -0500

    Add files via upload

---
+++ exercises/game05/Assets/Script/Target.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    
+    public int currentHealth = 3;
+
+    public void Damage(int damageAmount)
+    {
+        
+        currentHealth -= damageAmount;
+
+        //Destroy target 
+        if (currentHealth <= 0) 
+        {
+            
+            gameObject.SetActive (false);
+        }
+    }
+}
