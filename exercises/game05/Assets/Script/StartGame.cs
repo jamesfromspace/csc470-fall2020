bc6392aa6377371b28a9f9e3e2f5498555694acc
James White <42709165+jamesfromspace@users.noreply.github.com>
2020-11-13 23:47:38 -0500

    update

---
+++ exercises/game05/Assets/Script/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine; 
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame: MonoBehaviour 
+{ 
+    
+    public void NextScene(string game05)
+    {
+        SceneManager.LoadScene("game05");
+    }  
+}
---
+++ exercises/game05/Assets/Script/Target.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    //GameManager gm;
+    public int currentHealth = 1;
+    
+    void Start()
+    {
+        //gm = GameObject.Find("GameManagerObj").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int damageAmount)
+    {
+        
+        currentHealth -= damageAmount;
+
+        //Destroy target 
+        if (currentHealth <= 0) 
+        {
+            
+            gameObject.SetActive (false);
+            //gm.IncreaseScore();
+        }
+    }
+}
