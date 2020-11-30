da71d54b1ffa4e49d3533acb68d87cf44299e4f2
James White <42709165+jamesfromspace@users.noreply.github.com>
2020-11-29 23:16:20 -0500

    added enemy follow

---
+++ exercises/final/Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieMovement : MonoBehaviour
+{
+
+    public float forwardSpeed = 1.0f;
+   // public float rotationSpeed = 1.0f;
+
+   // public float Speed 4.0f;
+    public Transform Player;
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = Vector3.MoveTowards(transform.position, Player.position, forwardSpeed * Time.deltaTime);
+        rigid.MovePosition(pos);
+        transform.LookAt(Player);
+
+    }
+
+    
+}
